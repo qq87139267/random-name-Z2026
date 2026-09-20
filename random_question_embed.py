78f450d79ef44e1a6432e3622ea13193738e22b8
qq87139267 <87139267@qq.com>
2026-09-20 16:01:00 +0800

    Update random_question_embed.py

--- random_question_embed.py
+++ random_question_embed.py
@@ -28,7 +28,7 @@ DEFAULT_DATA = {
 class RollCallApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("💪下一位~~就係你~~~☝️↗")
+        self.root.title("下一位~~就係你~~~")
         self.root.geometry("800x600")
         self.root.configure(bg="#1a1a2e")
         
